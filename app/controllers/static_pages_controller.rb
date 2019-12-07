68a950bec3a4ac958126eb7fb88172fa206e2a3a
mikesaurio <mikesaurio@gmail.com>
2019-12-07 17:50:37 -0600

    remove url path static

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :get_root_url
   def general
     @index= 0
   end
@@ -12,5 +13,14 @@ class StaticPagesController < ApplicationController
 
   def sanctioned
     @index = 3
+  end
+
+  private
+  def get_root_url
+    if Rails.env.development?
+      @root_url = "http://localhost:3000"
+    else
+      @root_url = "https://hackaton-anticorrupcion.herokuapp.com"
+    end
   end
 end
